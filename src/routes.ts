435c48bdb60037e0b28b5b07142cd31162ec61bf
Augusto Peiró <gusto.peiro@hotmail.com>
2024-11-09 19:16:31 -0300

    add models2

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
+
+router.post('/users', new CreateUserController().handle)
 
 router.get('/teste', (req: Request, res: Response) => {
     return res.json({ ok: true})
